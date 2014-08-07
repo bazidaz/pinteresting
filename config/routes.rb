2474719c8faab6ce9f0f450bcb0a6049e037bb81
Ray rai <123rayrai@gmail.com>
2014-08-07 05:38:23 +0000

    added forum link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "forum" => "pages#forum"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
